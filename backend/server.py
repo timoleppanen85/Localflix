e438aa68eb0f198f50dd91bcdba17d045b5e3eb3
timoleppanen85 <31985525+timoleppanen85@users.noreply.github.com>
2024-04-23 16:23:07 +0300

    Fixes

--- backend/server.py
+++ backend/server.py
@@ -39,7 +39,7 @@ def login_required(f):
 @app.after_request
 def add_cors_headers(response):
     response.headers.add(
-        "Access-Control-Allow-Origin", "https://localflix.onrender.com/"
+        "Access-Control-Allow-Origin", "https://localflix.onrender.com/login"
     )
     response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
     response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
